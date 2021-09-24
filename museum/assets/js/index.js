da9e0967b39fd962d9ae3be209ae8d8f46d32248
Taneros <fricentaurus@GMAIL.COM>
2021-09-24 17:37:46 +0300

    fix: style video section

--- museum/assets/js/index.js
+++ museum/assets/js/index.js
@@ -1,24 +1,23 @@
-const progress = document.querySelectorAll('.progress');
+const progress = document.querySelectorAll('.progress')
 
-progress.forEach(el => el.addEventListener('input', function () {
-  const value = this.value;
-  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #fff ${value}%, white 100%)`
-}))
+progress.forEach((el) =>
+  el.addEventListener('input', function () {
+    const value = this.value
+    this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #fff ${value}%, white 100%)`
+  })
+)
 
-const card = document.querySelectorAll('.card');
-const cardBr = document.querySelectorAll('.br-card');
+const card = document.querySelectorAll('.card')
+const cardBr = document.querySelectorAll('.br-card')
 
-
-card.forEach(((el, i) => {
+card.forEach((el, i) => {
   el.addEventListener('mouseover', function () {
     cardBr[i].classList.add('hover')
   })
+})
 
-}))
-
-card.forEach(((el, i) => {
+card.forEach((el, i) => {
   el.addEventListener('mouseout', function () {
     cardBr[i].classList.remove('hover')
   })
-
-}))
+})
