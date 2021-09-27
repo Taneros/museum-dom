673d103728af168a8a3491c7efeb4258a6b35e6b
Taneros <fricentaurus@GMAIL.COM>
2021-09-27 13:29:07 +0300

    feat: js change gallery pic on reload

--- museum/assets/js/index.js
+++ museum/assets/js/index.js
@@ -1,3 +1,4 @@
+// import './galery.js'
 const progress = document.querySelectorAll('.progress')
 
 progress.forEach((el) =>
@@ -20,4 +21,39 @@ card.forEach((el, i) => {
   el.addEventListener('mouseout', function () {
     cardBr[i].classList.remove('hover')
   })
+})
+
+// gallery functionality
+const galleryInnerContainer = document.querySelector('.gallery__inner-container')
+
+const picArr = [
+  'assets/img/galery/galery1.jpg',
+  'assets/img/galery/galery2.jpg',
+  'assets/img/galery/galery3.jpg',
+  'assets/img/galery/galery4.jpg',
+  'assets/img/galery/galery5.jpg',
+  'assets/img/galery/galery6.jpg',
+  'assets/img/galery/galery7.jpg',
+  'assets/img/galery/galery8.jpg',
+  'assets/img/galery/galery9.jpg',
+  'assets/img/galery/galery10.jpg',
+  'assets/img/galery/galery11.jpg',
+  'assets/img/galery/galery12.jpg',
+  'assets/img/galery/galery13.jpg',
+  'assets/img/galery/galery14.jpg',
+  'assets/img/galery/galery15.jpg',
+]
+
+function shuffle(picArr) {
+  picArr.sort(() => Math.random() - 0.5)
+}
+
+shuffle(picArr)
+
+picArr.map((picArr, index) => {
+  const img = document.createElement('img')
+  img.classList.add('gallery__img')
+  img.src = picArr
+  img.alt = ``
+  galleryInnerContainer.append(img)
 })
