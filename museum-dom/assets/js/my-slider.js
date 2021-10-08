3d4f44186f89adc8ff1eefe6258776133b2d91a6
Taneros <fricentaurus@GMAIL.COM>
2021-10-08 20:11:57 +0300

    fix: slider begin num

--- museum-dom/assets/js/my-slider.js
+++ museum-dom/assets/js/my-slider.js
@@ -1,45 +1,50 @@
 console.log('my-slider.js is linked successfully')
 
-// const slider_box = document.querySelectorAll('.slider__box')
-// console.log(slider_box)
-// const slider_box_parent = document.querySelector('.welcome__slider-controls__pagination')
-// console.log(slider_box_parent)
-
-// slider_box.forEach((el) =>
-//   el.addEventListener('click', () => {
-//     console.log()
-//   })
-// )
-
-// slider_box_parent.addEventListener('click', (e) => {
-//   console.log(e.target)
-//   if (e.target.classList.contains('slider__box')) {
-//     console.log(1)
-//     console.log(e.target.dataset.index)
-//   }
-// })
+import Swiper from 'https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js'
 
 const swiperSettings = {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
-
-  // If we need pagination
-  // pagination: {
-  //   el: '.welcome__slider-controls__pagination',
-  //   type: 'fraction',
-  // },
+  initialSlide: 4,
+  enabled: true,
 
   // Navigation arrows
   navigation: {
     nextEl: '.welcome__slider-controls__right-arrow',
     prevEl: '.welcome__slider-controls__left-arrow',
   },
-
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
+  pagination: {},
+  // pagination: {
+  //   el: '.progress-bar',
+  //   type: 'bullets',
+  //   progressbarFillClass: '.progress-bar',
   // },
 }
 
-export default swiperSettings
+const swiper = new Swiper('.swiper', swiperSettings)
+const slider_box = document.querySelectorAll('.slider__box')
+const slider_box_parent = document.querySelector('.welcome__slider-controls__pagination')
+
+slider_box.forEach((el) => el.addEventListener('click', () => {}))
+
+slider_box_parent.addEventListener('click', (e) => {
+  if (e.target.classList.contains('slider__box')) {
+    swiper.slideTo(e.target.dataset.index)
+  }
+})
+
+// swiper.slideTo(0)
+swiper.onAny(updateBox)
+
+function updateBox() {
+  console.log('swiper.activeIndex', swiper.activeIndex)
+  const currentIndex = swiper.activeIndex % 5
+  console.log('currentIndex', currentIndex)
+  slider_box.forEach((el, idx) => {
+    el.classList.toggle('active-box', idx === currentIndex)
+  })
+  document.querySelector('#slider__current').textContent = `0${currentIndex + 1}`
+}
+
+// export default swiper
